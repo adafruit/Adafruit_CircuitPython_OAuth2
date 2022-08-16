cd896e2dc851e942b3cf71d98e5100bab7011c74
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_oauth2.py
+++ adafruit_oauth2.py
@@ -31,7 +31,7 @@ except ImportError:
 # imports
 import time
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_OAuth2.git"
 
 # Google's authorization server
